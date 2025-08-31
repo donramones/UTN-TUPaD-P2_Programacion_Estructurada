700b0c6ac4bd6bb1f83c99d77ac2d9bc79aaef7a
donramones <agusrivarolaa00@gmail.com>
2025-08-31 19:52:54 -0300

    ejerciciocuatro

---
+++ src/main/java/com/mycompany/utn/tupad/p2_programacion_estructurada/UTNTUPaDP2_Programacion_Estructurada.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.utn.tupad.p2_programacion_estructurada;
+
+/**
+ *
+ * @author rivar
+ */
+public class UTNTUPaDP2_Programacion_Estructurada {
+
+    public static void main(String[] args) {
+        System.out.println("hol World!");
+    }
+}
