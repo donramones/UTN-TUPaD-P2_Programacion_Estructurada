adbdb9f2cfec558dd7687e3c4919891f690b66ab
donramones <agusrivarolaa00@gmail.com>
2025-09-01 00:05:53 -0300

    ejercicios_7_unidad2

--- src/main/java/ejercicio_7.java
+++ src/main/java/ejercicio_7.java
@@ -7,6 +7,18 @@
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class ejercicio_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nota;
+        
+        do{
+            System.out.print("Ingrese una nota entre 0 y 10: ");
+            nota = sc.nextInt();
+        }while (nota < 0 || nota > 10);
+        System.out.println("La nota ingresada es: " + nota);
+        
+    }
     
 }
