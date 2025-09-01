eb0a9cdf9610e1163075221c79274fb55abd5950
donramones <agusrivarolaa00@gmail.com>
2025-08-31 21:44:27 -0300

    ejercicios_unidad2

--- src/main/java/ejercicio_1.java
+++ src/main/java/ejercicio_1.java
@@ -7,6 +7,18 @@
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class ejercicio_1 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un año: ");
+        int año = sc.nextInt();
+        if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0)) {
+            System.out.println("El año " + año + " es bisiesto.");
+        }else{
+            System.out.println("El año " + año + " no es bisiesto.");
+        }
+    }
     
 }
