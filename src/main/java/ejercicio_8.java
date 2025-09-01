0f30aa109dffea0165799ca7de7c332a936e6265
donramones <agusrivarolaa00@gmail.com>
2025-09-01 00:14:20 -0300

    ejercicios_8_unidad2

--- src/main/java/ejercicio_8.java
+++ src/main/java/ejercicio_8.java
@@ -7,6 +7,24 @@
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class ejercicio_8 {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double precioBase, impuesto, descuento;
+        double precioFinal;
+        System.out.print("Ingrese el precio base del producto: ");
+        precioBase = Integer.parseInt(input.nextLine());
+        System.out.print("Ingrese el impuesto en porcentaje (Ejemplo: 10 para 10%): ");
+        impuesto = Integer.parseInt(input.nextLine());
+        System.out.print("Ingrese el descuento en porcentaje (Ejemplo: 5 para 5%): ");
+        descuento = Integer.parseInt(input.nextLine());
+        System.out.println("El precio final del producto es: " + calcularPrecioFinal(precioBase, impuesto, descuento));
+        
+         
+     }
+     static double calcularPrecioFinal(double base, double impuesto, double descuento) {
+         return base + (base * (impuesto/100.0)) - (base * (descuento/100.0));
+     }
     
 }
