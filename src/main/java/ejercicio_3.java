ef8d996e845c6102e99c9fe52737d6afd45bddff
donramones <agusrivarolaa00@gmail.com>
2025-08-31 23:04:02 -0300

    ejercicios_3_unidad2

--- src/main/java/ejercicio_3.java
+++ src/main/java/ejercicio_3.java
@@ -7,6 +7,25 @@
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class ejercicio_3 {
-    
+    public static void main(String[] args) {
+        // Escribe un programa en Java que solicite al usuario su edad y clasifique su etapa de vida
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.print("Ingrese su edad: ");
+        int edad = sc.nextInt();
+        
+        if (edad < 12) {
+            System.out.println("Eres un ninio/a");
+        }else if (edad >= 12 && edad <= 17) {
+            System.out.println("Eres un adolescente");
+        }else if (edad >= 18 && edad <= 59) {
+            System.out.println("Eres un Adulto/a");
+        }else{
+            System.out.println("Eres un/a Adulto/a mayor");
+        }
+         
+         
+    }
 }
