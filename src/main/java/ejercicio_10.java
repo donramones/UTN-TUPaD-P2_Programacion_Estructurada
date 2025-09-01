05037bf543602ac28b44269ecaf504f9477074bc
donramones <agusrivarolaa00@gmail.com>
2025-09-01 00:29:31 -0300

    ejercicios_10_unidad2

--- src/main/java/ejercicio_10.java
+++ src/main/java/ejercicio_10.java
@@ -7,6 +7,30 @@
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class ejercicio_10 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Ingrese el stock actual del producto: ");
+        int stockActual = sc.nextInt();
+
+        System.out.print("Ingrese la cantidad de productos vendidos: ");
+        int cantidadVendida = sc.nextInt();
+
+        System.out.print("Ingrese la cantidad de productos recibidos: ");
+        int cantidadRecibida = sc.nextInt();
+
+       
+        int nuevoStock = actualizarStock(stockActual, cantidadVendida, cantidadRecibida);
+
+        
+        System.out.println("El nuevo stock es: " + nuevoStock);
+    }
+    public static int actualizarStock(int stockActual, int cantidadVendida, int cantidadRecibida) {
+        return stockActual - cantidadVendida + cantidadRecibida;
+                
+                
+    }
 }
