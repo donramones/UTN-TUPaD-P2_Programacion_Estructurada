dedbc9678d433033213390a06d8f0ff8ef28194b
donramones <agusrivarolaa00@gmail.com>
2025-08-31 23:50:29 -0300

    ejercicios_5_unidad2

--- src/main/java/ejercicio_5.java
+++ src/main/java/ejercicio_5.java
@@ -7,6 +7,22 @@
  *
  * @author rivar
  */
+import java.util.Scanner;
 public class ejercicio_5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = 1;
+        int numFinal = 0;
+        System.out.println("Ingrese numeros enteros (ingrese 0 para terminar)");
+        while (num != 0) {
+            System.out.print("Ingrese otro numero entero (0 para terminar): ");
+            num = Integer.parseInt(input.nextLine());
+            if (num % 2 == 0) {
+                numFinal += num;
+            }
+            
+        }
+        System.out.println("La suma de los numeros pares es: " + numFinal);
+    }
     
 }
